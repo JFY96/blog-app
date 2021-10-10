0f9426eb43ef1542d1d0833dcbcb5596c2bad2ee
JFY96 <36199863+JFY96@users.noreply.github.com>
2021-10-10 19:11:04 +0100

    Initial Commit - Set up React Project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	resolve: {
+		modules: [path.join(__dirname, 'src'), 'node_modules'],
+		alias: {
+		  react: path.join(__dirname, 'node_modules', 'react'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					},
+				}
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+		  template: './src/index.html',
+		}),
+	]
+};
